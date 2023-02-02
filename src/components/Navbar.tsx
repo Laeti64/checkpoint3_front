5870c2ac9906e098daacbb9fb41d960e7e5660ed
Pupuce-64 <laeti.decandt@gmail.com>
2023-02-02 15:19:44 +0100

    gestion du log in ou log out dans navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,30 @@
+import { useAuth } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
 import React from "react";
 
-type Props = {};
-
-export default function Navbar({}: Props) {
-  return <div className="invisible">Navbar</div>;
+export default function Navbar() {
+  const router = useRouter();
+  const { isAuth, signOut } = useAuth();
+  const handleClick = (isAuth: boolean) => {
+    isAuth ? signOut() : router.push("/auth/signin");
+  };
+  return (
+    <div className="flex justify-end ">
+      {isAuth ? (
+        <button
+          className="text-white font-bold mr-5 mt-5"
+          onClick={() => handleClick(isAuth)}
+        >
+          LOG OUT
+        </button>
+      ) : (
+        <button
+          className="text-white font-bold mr-5 mt-5"
+          onClick={() => handleClick(isAuth)}
+        >
+          LOG IN{" "}
+        </button>
+      )}
+    </div>
+  );
 }
